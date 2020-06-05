1b04ae22907c9e9609bfb0a81715c6e00fb4c99b
Stand <dtc15hd3401990046@ictu.edu.vn>
2020-06-05 20:53:19 +0700

    update

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -58,7 +58,7 @@
         </ul>
       </nav>
       <nuxt-link
-        to="/auth/login"
+        to="/login"
         class="bg-teal-500 hover:bg-teal-600 text-white font-bold py-2 px-4 rounded"
       >
         Đăng nhập
